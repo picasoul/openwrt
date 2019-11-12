bd9038419dc498eba8ec4287d0f8fe19d347e3e0
Luochongjun <luochongjun@gl-inet.com>
2019-11-12 10:23:31 +0800

    enable target e750

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -11,3 +11,17 @@ define Device/glinet_gl-ar300m-nand
   IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
 endef
 #TARGET_DEVICES += glinet_gl-ar300m-nand
+
+define Device/glinet_gl-e750
+  ATH_SOC := qca9531
+  DEVICE_TITLE := GL-E750
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport
+  KERNEL_SIZE := 2048k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  VID_HDR_OFFSET := 512
+  IMAGES += factory.ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar
+  IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+endef
+TARGET_DEVICES += glinet_gl-e750
